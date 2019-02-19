538db1a611cb49c02c5cd555b38276cfa5ce1c3a
Yun Park <hanlsin@gmail.com>
2019-02-19 02:17:33 +0000

    update minihorde formula

--- minihorde.rb
+++ minihorde.rb
@@ -2,7 +2,7 @@ class MiniHorde < Formula
   desc "MiniHorde binary"
   homepage "http://github.com/hanlsin/homebrew-tap"
   url "https://github.com/hanlsin/homebrew-tap/blob/master/dist/minihorde/minihorde-0.0.1.tar.gz"
-  sha256 "2f1d3078afb4adbdd92a3eab1f35d9d20ec3e7298a92b7c11c59218afe010455"
+  sha256 "88b0fead464c1dfcd6e343bfe2e3f40bd358ec9f2a57bf98943c58d3194dde06"
   version "0.0.1"
 
   def install
