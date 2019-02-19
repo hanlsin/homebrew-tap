979d09603490070ba001fc08a5c483da5ba92e9b
Yun Park <hanlsin@gmail.com>
2019-02-19 01:23:14 +0000

    change formula to test properly

--- minihorde.rb
+++ minihorde.rb
@@ -5,12 +5,13 @@ class Minihorde < Formula
   homepage ""
   url "https://github.com/hanlsin/homebrew-tap/blob/master/dist/minihorde/minihorde-0.0.1.tar.gz"
   sha256 "69b7b4425605a150a55441d5142ced3679bdbd73c59c0e8ffa215716d44d43f5"
+  version "0.0.1"
 
   def install
     bin.install "minihorde"
   end
 
   test do
-    system "false"
+    system("minihorde", "version")
   end
 end
