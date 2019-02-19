64ba12a5621cb98160b243721dc1effbe221e135
Yun Park <hanlsin@gmail.com>
2019-02-19 01:39:23 +0000

    modify tar file to release url

--- minihorde.rb
+++ minihorde.rb
@@ -1,11 +1,10 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
-class Minihorde < Formula
-  desc ""
+class HomebrewTap < Formula
+  desc "Homebrew tap for custom formulas."
   homepage ""
-  url "https://github.com/hanlsin/homebrew-tap/blob/master/dist/minihorde/minihorde-0.0.1.tar.gz"
-  sha256 "69b7b4425605a150a55441d5142ced3679bdbd73c59c0e8ffa215716d44d43f5"
-  version "0.0.1"
+  url "https://github.com/hanlsin/homebrew-tap/releases/download/minihorde-0.0.1/minihorde-0.0.1.tar.gz"
+  sha256 "6ce731853561d4d088b4642078ed9b70a4a0983ad0a78805376387f6e3ed273f"
 
   def install
     bin.install "minihorde"
