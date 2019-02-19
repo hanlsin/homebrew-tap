8c01ec85041552f18e8c8eb946cc1d8e943cd127
Yun Park <hanlsin@gmail.com>
2019-02-19 00:17:49 +0000

    add formula

---
+++ Formula/minihorde.rb
@@ -0,0 +1,29 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Minihorde < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/hanlsin/homebrew-tap/dist/minihorde"
+  version ""
+  sha256 ""
+  # depends_on "cmake" => :build
+
+  def install
+    # create symlink to script in /usr/local/bin
+    bin.install "minihorde"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test minihorde`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
